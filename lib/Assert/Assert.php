2263b7d61ede940f11074fb40058bd17207ea8fb
Jaime Pérez <jaimepc@gmail.com>
2020-06-12 17:08:12 +0200

    Fix a few issues
    is_subclass_of() must be called with Throwable, not Exception, or passing an Exception won't work.
    
    Also, we cannot check for our own static methods in __callStatic(), as those will be called directly.
    
    Instead of using a static property, it's just easier to avoid having an state in a purely static class, and therefore catching any exception (shall it occur), and "casting" it to whatever exception we need.

--- lib/Assert/Assert.php
+++ lib/Assert/Assert.php
@@ -6,7 +6,7 @@ namespace SimpleSAML\Assert;
 
 use BadMethodCallException;
 use Exception;
-use Webmozart\Assert as Webmozart;
+use Webmozart\Assert\Assert as Webmozart;
 
 /**
  * Webmozart\Assert wrapper class
@@ -14,16 +14,8 @@ use Webmozart\Assert as Webmozart;
  * @author Tim van Dijen, <tvdijen@gmail.com>
  * @package simplesamlphp/assert
  */
-final class Assert extends Webmozart\Assert
+final class Assert 
 {
-    /**
-     * Exception to throw when an assertion failed.
-     *
-     * @var class-string
-     */
-    protected static $exceptionClass = AssertionFailedException::class;
-
-
     /**
      * @param string $name
      * @param array $arguments
@@ -32,57 +24,49 @@ final class Assert extends Webmozart\Assert
     public static function __callStatic($name, $arguments)
     {
         // Handle Exception-parameter
+        $exception = \InvalidArgumentException::class;
         $last = end($arguments);
-        if (is_string($last) && class_exists($last) && is_subclass_of($last, Exception::class)) {
-            self::$exceptionClass = $last;
+        echo $last."\n\n";
+        if (is_string($last) && class_exists($last) && is_subclass_of($last, \Throwable::class)) {
+            $exception = $last;
 
             array_pop($arguments);
         }
 
-        if ('nullOr' === substr($name, 0, 6)) {
-            if (null !== $arguments[0]) {
-                $method = lcfirst(substr($name, 6));
-                call_user_func_array(['static', $method], $arguments);
+
+        try {
+            // handle nullOr* methods
+            if ('nullOr' === substr($name, 0, 6)) {
+                if (null !== $arguments[0]) {
+                    $method = lcfirst(substr($name, 6));
+                    call_user_func_array([Webmozart::class, $method], $arguments);
+                }
+
+                return;
             }
 
-            return;
-        }
+            // handle all* methods
+            if ('all' === substr($name, 0, 3)) {
+                static::isIterable($arguments[0]);
 
-        if ('all' === substr($name, 0, 3)) {
-            static::isIterable($arguments[0]);
+                $method = lcfirst(substr($name, 3));
+                $args = $arguments;
 
-            $method = lcfirst(substr($name, 3));
-            $args = $arguments;
+                foreach ($arguments[0] as $entry) {
+                    $args[0] = $entry;
 
-            foreach ($arguments[0] as $entry) {
-                $args[0] = $entry;
+                    call_user_func_array([Webmozart::class, $method], $args);
+                }
 
-                call_user_func_array(['static', $method], $args);
+                return;
             }
-
-            return;
-        }
-
-        // Handle locally added assertions
-        if (method_exists(static::class, $method)) {
-            call_user_func_array([static::class, $method], $arguments);
+        
+            // all other methods
+            call_user_func_array([Webmozart::class, $name], $arguments);
+        } catch (\InvalidArgumentException $e) {
+            throw new $exception($e->getMessage());
         }
 
         throw new BadMethodCallException('No such method: ' . $name);
-    }
-
-
-    /**
-     * @param string $message
-     * @return void
-     *
-     * @throws \SimpleSAML\Assert\AssertionFailedException
-     *
-     * @psalm-pure this method is not supposed to perform side-effects
-     * @psalm-suppress InvalidStringClass
-     */
-    protected static function reportInvalidArgument($message)
-    {
-        throw new self::$exceptionClass($message);
     }
 }
