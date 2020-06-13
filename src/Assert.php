aaf9d498ba3c7935fc3e5f61d5b7212183c1bfec
Tim van Dijen <tvdijen@gmail.com>
2020-06-13 12:55:40 +0200

    Cleanup

--- src/Assert.php
+++ src/Assert.php
@@ -33,41 +33,11 @@ final class Assert
             array_pop($arguments);
         }
 
-
         try {
-            // handle nullOr* methods
-            if ('nullOr' === substr($name, 0, 6)) {
-                if (null !== $arguments[0]) {
-                    $method = lcfirst(substr($name, 6));
-                    call_user_func_array([Webmozart::class, $method], $arguments);
-                }
-
-                return;
-            }
-
-            // handle all* methods
-            if ('all' === substr($name, 0, 3)) {
-                static::isIterable($arguments[0]);
-
-                $method = lcfirst(substr($name, 3));
-                $args = $arguments;
-
-                foreach ($arguments[0] as $entry) {
-                    $args[0] = $entry;
-
-                    call_user_func_array([Webmozart::class, $method], $args);
-                }
-
-                return;
-            }
-
-            // all other methods
             call_user_func_array([Webmozart::class, $name], $arguments);
             return;
         } catch (InvalidArgumentException $e) {
             throw new $exception($e->getMessage());
         }
-
-        throw new BadMethodCallException('No such method: ' . $name);
     }
 }
