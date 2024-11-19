c2ee7d7813cbf06c8bf96118a5e177c4acea33a5
Tim van Dijen <tvdijen@gmail.com>
2024-11-19 18:21:36 +0100

    Add support for enums in error message

---
+++ tests/Utils/TestEnum.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SimpleSAML\Test\Utils;
+
+// Declare a simple enumeration
+enum TestEnum
+{
+    case PHPCS;
+    case PHPStan;
+    case PHPUnit;
+    case Psalm;
+}
